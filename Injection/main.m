5bb347db24a81637b3b111b1e6a76784f3512438
RbBtSn0w <hamiltonsnow@gmail.com>
2017-11-02 16:35:55 +0800

    Initial Commit

---
+++ Injection/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Injection
+//
+//  Created by Snow.y.wu on 11/2/17.
+//  Copyright © 2017 rbbtsn0w. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
